4bec95f0a36a881dc7eb031714ea47bde811ffe4
Ulrich HOUNGBO <houngbou@gmail.com>
2023-10-24 20:03:27 +0100

    class FIFOCache that inherits from BaseCaching and discard last data in dictionary if it's more than cache_data

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+"""
+LIFO Caching
+"""
+from collections import deque
+
+BaseCache = __import__("base_caching").BaseCaching
+
+
+class LIFOCache(BaseCache):
+    """Init"""
+
+    def __init__(self):
+        """Constructor"""
+        super().__init__()
+        self.queue = deque()
+
+    def put(self, key, item):
+        """Add an item in the cache"""
+        if key and item:
+            if key in self.cache_data:
+                self.queue.remove(key)
+            elif self.is_full():
+                self.evict()
+        self.queue.append(key)
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """Get an item by key"""
+        return self.cache_data.get(key, None)
+
+    def is_full(self):
+        """If the number of items in self.cache_data is higher that
+        BaseCaching.MAX_ITEMS
+        """
+        return len(self.cache_data) >= self.MAX_ITEMS
+
+    def evict(self):
+        """you must print DISCARD: with the key discarded and following by a
+        new line
+        """
+        popped = self.queue.pop()
+        del self.cache_data[popped]
+        print("DISCARD: " + str(popped))
