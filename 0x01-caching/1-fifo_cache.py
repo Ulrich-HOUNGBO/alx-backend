4a5f2e3744471a0f5037ab8a0c2ac83e4210d2aa
Ulrich HOUNGBO <houngbou@gmail.com>
2023-10-24 19:33:35 +0100

    class FIFOCache that inherits from BaseCaching and discard first data in dictionary if it's more than cache_data

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+""" FIFO caching """
+from base_caching import BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """ Class FIFOCache"""
+
+    def __init__(self):
+        """Constructor"""
+        super().__init__()
+        self.queue = []
+
+    def put(self, key, item):
+        """Assign into dictionary using FIFO"""
+        if key and item:
+            if key in self.cache_data:
+                self.cache_data[key] = item
+                return
+            if len(self.cache_data) >= self.MAX_ITEMS:
+                first = self.queue.pop(0)
+                del self.cache_data[first]
+                print("DISCARD: {}".format(first))
+            self.queue.append(key)
+        return None
+
+    def get(self, key):
+        """Return value of cache_data linked to key"""
+        if key in self.cache_data:
+            return self.cache_data[key]
+        return None
