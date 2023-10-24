d2191867ec41bcdf9400ed9c30284e65bf9dc52a
Ulrich HOUNGBO <houngbou@gmail.com>
2023-10-24 19:09:47 +0100

    BasicCache class that inherits BaseCaching

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,21 @@
+# /usr/bin/env python3
+
+""" Basic dictionary """
+
+from base_caching import BaseCaching
+
+
+class BasiCache(BaseCaching):
+    """ Class BasicCache"""
+
+    def put(self, key, item):
+        """Assign into dictionary"""
+        if key and item:
+            self.cache_data[key] = item
+        return None
+
+    def get(self, key):
+        """Return value of cache_data linked to key"""
+        if key in self.cache_data:
+            return self.cache_data[key]
+        return None
