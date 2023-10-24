52ecf46adeb0db7f5fcb5b1c36ae2007dadf909c
Ulrich HOUNGBO <houngbou@gmail.com>
2023-10-24 20:15:08 +0100

    class FIFOCache that inherits from BaseCaching and discard last data in dictionary if it's more than cache_data

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -1,23 +1,27 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
+""" 2. LIFO Caching
+"""
 
-"""
-LIFO Caching
-"""
 from collections import deque
 
-BaseCache = __import__("base_caching").BaseCaching
+BaseCaching = __import__("base_caching").BaseCaching
 
 
-class LIFOCache(BaseCache):
-    """Init"""
+class LIFOCache(BaseCaching):
+    """ You must use self.cache_data - dictionary from the parent class
+    BaseCaching
+    """
 
     def __init__(self):
-        """Constructor"""
+        """ Init
+        """
         super().__init__()
         self.queue = deque()
 
     def put(self, key, item):
-        """Add an item in the cache"""
+        """ Must assign to the dictionary self.cache_data the item value for
+        the key
+        """
         if key and item:
             if key in self.cache_data:
                 self.queue.remove(key)
@@ -27,18 +31,19 @@ class LIFOCache(BaseCache):
             self.cache_data[key] = item
 
     def get(self, key):
-        """Get an item by key"""
+        """ Must return the value in self.cache_data linked to key.
+        """
         return self.cache_data.get(key, None)
 
     def is_full(self):
-        """If the number of items in self.cache_data is higher that
+        """ If the number of items in self.cache_data is higher that
         BaseCaching.MAX_ITEMS
         """
         return len(self.cache_data) >= self.MAX_ITEMS
 
     def evict(self):
-        """you must print DISCARD: with the key discarded and following by a
-        new line
+        """ you must print DISCARD: with the key discarded and following by a
+        new line -pop-
         """
         popped = self.queue.pop()
         del self.cache_data[popped]
