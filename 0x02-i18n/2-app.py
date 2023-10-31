71892ed58afe9433ba14822471ff3d33ebcc452a
Ulrich HOUNGBO <houngbou@gmail.com>
2023-10-31 17:58:25 +0100

    determine the best match with our supported languages

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -1,37 +1,35 @@
 #!/usr/bin/env python3
-
-"""Configure Flask-Babel to support EN & FR"""
-
+""" Basic Flask app, Basic Babel setup, Get locale from request """
 from flask import Flask, render_template, request
-
 from flask_babel import Babel
 
 app = Flask(__name__)
-
 babel = Babel(app)
+""" instantiate the Babel object """
 
 
 class Config(object):
-    """Config class for app"""
-    LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = "en"
-    BABEL_DEFAULT_TIMEZONE = "UTC"
+    """ config class """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
 app.config.from_object(Config)
-
-
-def get_locale():
-    """Get locale"""
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
+""" Use that class as config for Flask app """
 
 
 @app.route('/')
 def root():
-    """ Basic Flask App
-    """
-    return render_template('2-index.html')
+    """ basic Flask app """
+    return render_template("2-index.html")
 
 
-if __name__ == '__main__':
-    app.run(host='0000000', port=5000, debug=True)
+@babel.localeselector
+def get_locale():
+    """ to determine the best match with our supported languages """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+if __name__ == "__main__":
+    app.run()
