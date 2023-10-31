0939d58dc8850808409322ddcbd092b84406fd8e
Ulrich HOUNGBO <houngbou@gmail.com>
2023-10-31 17:46:33 +0100

    instantiate the Babel object in your app. Store it in a module-level variable named babel

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+"""Configure Flask-Babel to support EN & FR"""
+
+from flask import Flask, render_template, request
+
+from flask_babel import Babel
+
+app = Flask(__name__)
+
+babel = Babel(app)
+
+
+class Config(object):
+    """Config class for app"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+
+
+@app.route('/')
+def root():
+    """ Basic Flask App
+    """
+    return render_template('1-index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0000000', port=5000, debug=True)
